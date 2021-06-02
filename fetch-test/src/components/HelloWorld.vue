7dfc91f198f29a9281355b4d514d99a45cacf034
Olioni Taumoepeau <o.taumoepeau@internal.taiwananga.co.nz>
2021-06-02 15:03:01 +1200

    new repo, started vue project

---
+++ fetch-test/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <button @click="showMaleCharacters()">MALE</button>
+    <button @click="showFemaleCharacters()">FEMALE</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+
+  },
+  data() {
+    return {
+      people: {},
+      size: 0
+    }
+  },
+  created() {
+  // Simple GET request using fetch
+    fetch("https://swapi.dev/api/people/")
+      .then(response => response.json())
+      .then(data => this.people = data);
+  },
+  methods: {
+    showMaleCharacters() {
+      console.log(this.people.results)
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
